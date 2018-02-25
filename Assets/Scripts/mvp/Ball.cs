6582095428cc9563213b991316de1ab349603dc2
Adsith <carlosalvarez1512@gmail.com>
2018-02-25 19:31:51 +0100

    Add ball with rigid body Add pins with rigid body Add script to manipulate the ball

---
+++ Assets/Scripts/mvp/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float thrust = 0.5f;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(transform.forward * thrust);
+        }            
+    }
+}
