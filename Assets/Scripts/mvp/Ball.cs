795fa5cb8440dee45345eea36cb891067398564e
Unknown <carlosalvarez1512@gmail.com>
2018-03-05 22:15:19 +0100

    Add the ability to move the ball lefat and right

--- Assets/Scripts/mvp/Ball.cs
+++ Assets/Scripts/mvp/Ball.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-    public float thrust = 0.5f;
+    private const float MoveSpeed = 0.1f; 
+
+    public float thrust = 1300f;
     public Rigidbody rb;
+    public States.BallStates currentState;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        currentState = States.BallStates.Move;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (currentState == States.BallStates.Move)
+        {
+            this.MoveControl();
+        }            
+    }
+
+    // Called on the update when the state is Move
+    private void MoveControl ()
+    {
         if (Input.GetKeyDown("space"))
         {
             rb.AddForce(transform.forward * thrust);
-        }            
+            this.currentState = States.BallStates.Roll;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= MoveSpeed;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x += MoveSpeed;
+            this.transform.position = position;
+        }
     }
 }
---
+++ Assets/Scripts/mvp/States.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class States : MonoBehaviour {
+
+    public enum BallStates { Move, Rotate, Spin, Force, Roll }
+}
